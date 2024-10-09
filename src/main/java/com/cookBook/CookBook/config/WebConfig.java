5b6261c7950bc77e3d33b7d51ad457be9771b7ea
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2024-10-09 20:18:40 +0200

    login/register working ok on backend and front

--- src/main/java/com/cookBook/CookBook/config/WebConfig.java
+++ src/main/java/com/cookBook/CookBook/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
