424c6589a565b7c3dc8ecfcc5589e891867e1cf5
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2024-11-14 13:13:57 +0100

    add frontend link

--- src/main/java/com/cookBook/CookBook/config/WebConfig.java
+++ src/main/java/com/cookBook/CookBook/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("http://127.0.0.1:5500", "https://foodbook-1.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowedHeaders("Content-Type", "Authorization")
